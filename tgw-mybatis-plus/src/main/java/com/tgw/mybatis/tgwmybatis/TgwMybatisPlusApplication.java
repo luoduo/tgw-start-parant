a8894963ead82cc81795b9721a50a021444a2257
tanggewei <luoduotang@163.com>
2019-11-28 10:05:09 +0800

    1

---
+++ tgw-mybatis-plus/src/main/java/com/tgw/mybatis/tgwmybatis/TgwMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.tgw.mybatis.tgwmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgwMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgwMybatisPlusApplication.class, args);
+    }
+
+}

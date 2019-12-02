873695233d090fb5c125601bee1e5c13833000a4
tanggewei <luoduotang@163.com>
2019-12-02 17:49:11 +0800

    redis-cache

---
+++ tgw-start-kafka/src/main/java/com/tgw/boot/tgwstartkafka/TgwStartKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.tgw.boot.tgwstartkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgwStartKafkaApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TgwStartKafkaApplication.class, args);
+    }
+    
+}
---
+++ tgw-start-kafka/src/test/java/com/tgw/boot/tgwstartkafka/TgwStartKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgw.boot.tgwstartkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgwStartKafkaApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}

a1121ca043d4196385938abcd233a61aac6bd08e
tanggewei <luoduotang@163.com>
2019-12-09 12:23:55 +0800

    sso

---
+++ tgw-start-wsdl/src/main/java/com/dbgo/webservicedemo/TgwStartWsdlApplication.java
@@ -0,0 +1,13 @@
+package com.dbgo.webservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgwStartWsdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgwStartWsdlApplication.class, args);
+    }
+
+}
